2592241ea6bb5d46a159ee372599a573c4992712
aanyos <85759900+aanyos@users.noreply.github.com>
2022-09-13 21:40:34 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(640, 360);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save("myFilterImage.png");
+}
+
